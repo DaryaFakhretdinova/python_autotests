a39ac54c9a681c8031db61cd849d6abd722a6a3b
DaryaFakhretdinova <fakhretdinova1991@yandex.ru>
2025-03-14 19:12:15 +0300

    Update test_response.py

--- python_project/tests/test_response.py
+++ python_project/tests/test_response.py
@@ -6,12 +6,12 @@ token = '802fc5c651ee0bc38f034cba6dcbab40'
 header = {'Content-Type':'application/json', 'trainer_token': token}
 trainer_id = '27915'
 
-def test_homework():
+def test_status_code_get_trainers():
     
     response = requests.get(url = f'{url}/trainers', headers = header)
     assert response.status_code == 200
 
-def test_homework_2():
+def test_my_name_in_get_trainers():
     
     response = requests.get(url = f'{url}/trainers?trainer_id=27915', headers = header)
     assert response.json()['data'][0]['trainer_name'] == 'Редли Флейм'
